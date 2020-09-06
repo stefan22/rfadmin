f3020247d66b7b185d0a9365c9976d6d324d3245
stefan22 <stefan.emailacct@gmail.com>
2020-09-06 19:46:33 +0100

    [withAuth] hoc comp udpate

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,11 +1,13 @@
 import app from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 import config from "./config";
 
 class Firebase {
   constructor() {
     app.initializeApp(config);
     this.auth = app.auth();
+    this.db = app.firestore();
   }
 
   // auth signup
---
+++ src/components/Session/withAuthorization.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import AuthUserContext from "./context";
+import { withFirebase } from "../Firebase";
+import { compose } from "recompose";
+import * as ROUTES from "../../helpers/constants";
+
+const withAuthorization = isAdmin => Component => {
+  class WithAuthorization extends React.Component {
+    componentDidMount() {
+      this.subscribe = this.props.firebase.auth.onAuthStateChanged(
+        authUser => {
+          if (!isAdmin(authUser)) {
+            this.props.history.push(ROUTES.SIGN_IN);
+          }
+        },
+      );
+    }
+    componentWillUnmount() {
+      this.subscribe();
+    }
+
+    render() {
+      return (
+        <AuthUserContext.Consumer>
+          {authUser =>
+            isAdmin(authUser) ? <Component {...this.props} /> : null
+          }
+        </AuthUserContext.Consumer>
+      );
+    }
+  } //WithAuthorization
+
+  return compose(withRouter, withFirebase)(WithAuthorization);
+};
+
+export default withAuthorization;
