5bb7437a23bc5dcce7ae85f0995f553aa1aa5070
stefan22 <stefan.emailacct@gmail.com>
2020-09-08 20:57:47 +0100

    [test] app

--- src/App.test.js
+++ src/App.test.js
@@ -5,37 +5,24 @@ import App from "./App";
 import MainRouter from "./router/MainRouter";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/styles";
-import { withAuthentication } from "./components/Session";
 
 
 describe("App component", () => {
   const wrapper = shallow(<App />);
-  beforeEach(() => {
-    return wrapper;
-  })
+  beforeEach(() => wrapper );
 
   it("renders App without crashing", () => {
     expect(wrapper).toHaveLength(1);
   });
 
   it("Main Router defined in App wrapper", () => {
-    //const wrapper = shallow(<App />);
     expect(wrapper.find(MainRouter)).toBeDefined();
   });
 
   it("ThemeProvider, Reset styles defined within App Component", () => {
-    //const wrapper = shallow(<App />);
     expect(wrapper.find(CssBaseline)).toBeDefined();
     expect(wrapper.find(ThemeProvider)).toBeDefined();
   });
-
-   it("Theme provider and Reset Css components defined within top wrapper", () => {
-    //const wrapper = shallow(<App />);
-    
-  });
-
-
-
 
 })
 
