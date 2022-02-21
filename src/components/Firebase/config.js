5a60e2b1123e38abbf39509bba31dbdec08a85e2
Stefano L <stefanosemail@protonmail.com>
2022-02-21 10:31:01 +0000

    Update config.js

--- src/components/Firebase/config.js
+++ src/components/Firebase/config.js
@@ -2,13 +2,13 @@ import fb from "../../fb";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBkm7qxmtzaxjqy8ePkiCjN-ZnxkVIdj0E",
-  authDomain: "dashboard-adeff.firebaseapp.com",
-  projectId: "dashboard-adeff",
-  storageBucket: "dashboard-adeff.appspot.com",
-  messagingSenderId: "375311919703",
-  appId: "1:375311919703:web:40900eefc0d467eb485112",
-  measurementId: "G-CKVC8WZENT"
+  apiKey: fb.API_KEY,
+  authDomain: fb.AUTH_DOMAIN,
+  projectId: fb.PROJECT_ID,
+  storageBucket: fb.STORAGE_BUCKET,
+  messagingSenderId: fb.MESSAGING_SENDER_ID,
+  appId: fb.APP_ID,
+  measurementId: fb.MEASUREMENT_ID,
 };
 
 
